23218bbe9d1f4d9a6bdfd64e4e52f2708a00796e
alexelwood14 <alex.elwood14@outlook.com>
2022-08-29 22:26:51 +0100

    Clean up

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
-Alex Elwood | June 2020
+# Mandelbrot Viewer
 
-Zoom In 			       -  W
-Zoom Out 			       -  S
-Move Up				       -  Up Arrow
-Move Down 			       -  Down Arrow
-Move Left			       -  Left Arrow
-Move Right			       -  Right Arrow
-Decrease Mandelbrot Formula Iterations -  R
-Increase Mandelbrot Formula Iterations -  F
-Terminate Program                      -  Esc
+A small project using OpenGL to view and navigate the [Mandelbrot Set](https://en.wikipedia.org/wiki/Mandelbrot_set).
+
+![Screenshot of the mandelbrot set](mandelbrot.png)
+
+## Controls
+
+| Action               | Button      |
+| -------------------- | ----------- |
+| Zoom In              | W           |
+| Zoom Out             | S           |
+| Move Up              | Up Arrow    |
+| Move Down            | Down Arrow  |
+| Move Left            | Left Arrow  |
+| Move Right           | Right Arrow |
+| Increase Iterations* | R           |
+| Decrease Iterations* | F           |
+| Terminate Program    | Esc         |
+
+*Maximum number of iterations used to determine if a coordinate is divergent or not. Increases/Decreases fractal detail.
--- rendering.py
+++ rendering.py
@@ -1,46 +1,46 @@
-#Alex Elwood | June 2020
+# Alex Elwood | June 2020
 
 import glfw
 import math
 from OpenGL.GL import *
 import OpenGL.GL.shaders
 import numpy
-import pyrr
+
 
 def initiate(resolution, fullscreen, double):
-    #Generates all rendering presets and performs OpenGL rendering setup functions
+    # Generates all rendering presets and performs OpenGL rendering setup functions
 
     glfw.init()
     data_size = 4
     line_length = 2
 
-    #Opens a windowed GLFW window
+    # Opens a windowed GLFW window
     if fullscreen:
         window = glfw.create_window(resolution[0], resolution[1], "Mandelbrot Set", glfw.get_primary_monitor(), None)
     else:
         window = glfw.create_window(resolution[0], resolution[1], "Mandelbrot Set", None, None)
 
-    #OpenGL and GLFW persets and shader initiation
+    # OpenGL and GLFW presets and shader initiation
     glfw.make_context_current(window)
     shader = create_shader(double)
     glEnable(GL_DEPTH_TEST)
     glClearColor(0, 0, 1.0, 1.0)
 
-    #Initiates all shader uniforms and places their access locations in a dictionary
-                 
-    #Presets all constant shader uniforms 
-    render_data = {"shader" : shader,
-                   "window" : window,
-                   "data_size" : data_size,
-                   "line_length" : line_length}
+    # Initiates all shader uniforms and places their access locations in a dictionary
+
+    # Presets all constant shader uniforms
+    render_data = {"shader": shader,
+                   "window": window,
+                   "data_size": data_size,
+                   "line_length": line_length}
 
     return render_data
 
 
 def create_shader(double):
-    #Defines the shaders and passes them OpenGL on the GPU
-    
-    #Defines the vertex shader including its uniforms and functions
+    # Defines the shaders and passes them OpenGL on the GPU
+
+    # Defines the vertex shader including its uniforms and functions
     vertex_shader = """
     #version 330
     in vec2 position;
@@ -50,7 +50,7 @@ def create_shader(double):
     }
     """
 
-    #Defines the fragment shader including its uniforms and functions
+    # Defines the fragment shader including its uniforms and functions
     if double:
         fragment_shader = """
         #version 400
@@ -137,19 +137,19 @@ def create_shader(double):
         }
         """
 
-    #Compile the shaders on the GPU
+    # Compile the shaders on the GPU
     shader = glCreateProgram()
     vertex_shader = OpenGL.GL.shaders.compileShader(vertex_shader, GL_VERTEX_SHADER)
     fragment_shader = OpenGL.GL.shaders.compileShader(fragment_shader, GL_FRAGMENT_SHADER)
 
-    #Shader initiation in OpenGL
+    # Shader initiation in OpenGL
     glAttachShader(shader, vertex_shader)
     glAttachShader(shader, fragment_shader)
     glLinkProgram(shader)
     glValidateProgram(shader)
     glUseProgram(shader)
 
-    #Garbage collection for the shaders on processor memory
+    # Garbage collection for the shaders on processor memory
     glDeleteShader(vertex_shader)
     glDeleteShader(fragment_shader)
 
@@ -157,29 +157,26 @@ def create_shader(double):
 
 
 def setup_buffer(render_data, vertices, surfaces):
-    #Uses OpenGL commands to create and setup the GPU buffers 
-    
-    #Formatting vertex data into singe numpy array for OpenGL compatability
-    vertices = numpy.array(vertices, dtype = numpy.float32)
+    # Uses OpenGL commands to create and setup the GPU buffers
 
+    # Formatting vertex data into singe numpy array for OpenGL compatability
+    vertices = numpy.array(vertices, dtype=numpy.float32)
 
-    #Creation and upload of data into the buffers stored on the GPU
+    # Creation and upload of data into the buffers stored on the GPU
     vertex_buffer = glGenBuffers(1)
     glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer)
     glBufferData(GL_ARRAY_BUFFER, render_data["data_size"] * len(vertices), vertices, GL_STATIC_DRAW)
 
-
-    #Setting of data locations within the buffer so OpenGL can access the data
+    # Setting of data locations within the buffer so OpenGL can access the data
     position = glGetAttribLocation(render_data["shader"], "position")
-    glVertexAttribPointer(position, 2, GL_FLOAT, GL_FALSE, render_data["data_size"] * render_data["line_length"], ctypes.c_void_p(0))
+    glVertexAttribPointer(position, 2, GL_FLOAT, GL_FALSE, render_data["data_size"] * render_data["line_length"],
+                          ctypes.c_void_p(0))
     glEnableVertexAttribArray(position)
 
+    # Formatting surface data into single numpy array for OpenGL compatability
+    surfaces = numpy.array(surfaces, dtype=numpy.uint32)
 
-    #Formatting surface data into single numpy array for OpenGL compatability
-    surfaces = numpy.array(surfaces, dtype = numpy.uint32)
-
-
-    #Clears and uploads the surface data into the surface buffer
+    # Clears and uploads the surface data into the surface buffer
     surface_buffer = glGenBuffers(1)
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, surface_buffer)
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, render_data["data_size"] * len(surfaces), surfaces, GL_STATIC_DRAW)
@@ -191,6 +188,7 @@ def setup_buffer(render_data, vertices, surfaces):
 
 def main():
     pass
+
 
 if __name__ == "__main__":
     main()
